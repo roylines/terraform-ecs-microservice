dc422c67b8d7f0286ff48de473eccdc9e8bcd9c4
roylines <roy@roylines.co.uk>
2018-03-19 13:56:46 +0000

    cleanup

--- variables.tf
+++ variables.tf
@@ -64,64 +64,8 @@ locals {
 }
 
 /*
-
-variable "vpc" {
-  description = "the vpc name"
-}
-
-variable "vpc_id" {
-  description = "the vpc id"
-}
-
-variable "region" {
-  description = "the region"
-}
-
-variable "subnets" {
-  description = "the subnets"
-}
-
 variable "private_zone_id" {
   description = "the r53 private zone id"
-}
-
-variable "cluster_id" {
-  description = "the cluster_id"
-}
-
-variable "microservice_elb_security_group_id" {
-  description = "the elb security group to use id"
-}
-
-variable "name" {
-  description = "the microservice name"
-}
-
-variable "version" {
-  description = "the microservice version"
-  default = "v1"
-}
-
-variable "image" {
-  description = "the docker container image to use"
-}
-
-variable "container_port" {
-  description = "the container port"
-  default = 8080
-}
-
-variable "port" {
-  description = "the microservice port"
-}
-
-variable "desired_count" {
-  description = "the number of microservices to provision"
-}
-
-variable "internal" {
-  description = "is the microservice internal only"
-  default = true
 }
 
 variable "acm_certificate_arn" {
